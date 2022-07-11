4526af4aecb4f4c43af70c0eb64d5a0ffb5daeac
chengkrilo <krilo_c@hotmail>
2022-07-11 17:17:10 +0800

    deal chinese font

--- main.go
+++ main.go
@@ -1,23 +1,47 @@
 package main
 
 import (
-	"fmt"
-
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
-func main() {
-	a := app.New()
-	w := a.NewWindow("Hello world")
-
-	w.SetContent(widget.NewLabel("Hello"))
-	// w.ShowAndRun()
-	w.Show()
-	a.Run()
-	tidyUp()
+type config struct {
+	EditWidge     *widget.Entry
+	PreviewWidget *widget.RichText
+	CurrentFile   fyne.URI
+	SaveMenuItem  *fyne.MenuItem
 }
 
-func tidyUp() {
-	fmt.Println("Exited")
+var cfg config
+
+func main() {
+	// create a fyne app
+	a := app.New()
+
+	//create a window for the app
+	win := a.NewWindow("Markdown")
+
+	//get the user interface
+	edit, preview := cfg.makeUI()
+
+	//set the content of the window
+	win.SetContent(container.NewHSplit(edit, preview))
+
+	//ToDo: show window and run app
+	win.Resize(fyne.Size{Width: 800, Height: 500})
+	win.CenterOnScreen()
+	win.ShowAndRun()
+}
+
+func (app *config) makeUI() (*widget.Entry, *widget.RichText) {
+	edit := widget.NewMultiLineEntry()
+	preview := widget.NewRichTextFromMarkdown("")
+	app.EditWidge = edit
+	app.PreviewWidget = preview
+
+	edit.OnChanged = preview.ParseMarkdown
+
+	return edit, preview
 }
